49dccc898d3bcbe60745793b88c83f3613e3637c
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-09-30 16:49:28 +0530

    added product-catalouge page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: [
-      'http://localhost:3001',
-      'storage.googleapis.com',
-      'https://timewatch2-0-311005204045.europe-west1.run.app'
-    ],
+      domains: [
+        'http://localhost:3001',
+        'storage.googleapis.com',
+        'https://timewatch2-0-311005204045.europe-west1.run.app'
+      ],
     remotePatterns: [
       {
         protocol: 'http', 
