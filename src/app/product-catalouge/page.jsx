78257e063088e07387d35475fa075a2026d35b8c
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-09-30 16:05:34 +0530

    added product-catalouge page

--- src/app/product-catalouge/page.jsx
+++ src/app/product-catalouge/page.jsx
@@ -10,7 +10,7 @@ export default function MyFlipBook() {
   const totalImages = 60;
   const imagePaths = Array.from(
     { length: totalImages },
-    (_, i) => `/images/flipbook/Artboard ${i + 1}@2x-100.jpg`
+    (_, i) => `../../flipbook/Artboard ${i + 1}@2x-100.jpg`
   );
 
   // State to store responsive dimensions
@@ -83,16 +83,78 @@ export default function MyFlipBook() {
         autoSize={true}
         usePortrait={true}
       >
-        {imagePaths.map((src, index) => (
+       <Image src="/images/flipbook/Artboard 1@2x-100.jpg" key={1} alt="Image 1" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 2@2x-100.jpg" key={2} alt="Image 2" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 3@2x-100.jpg" key={3} alt="Image 3" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 4@2x-100.jpg" key={4} alt="Image 4" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 5@2x-100.jpg" key={5} alt="Image 5" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 6@2x-100.jpg" key={6} alt="Image 6" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 7@2x-100.jpg" key={7} alt="Image 7" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 8@2x-100.jpg" key={8} alt="Image 8" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 9@2x-100.jpg" key={9} alt="Image 9" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 10@2x-100.jpg" key={10} alt="Image 10" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 11@2x-100.jpg" key={11} alt="Image 11" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 12@2x-100.jpg" key={12} alt="Image 12" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 13@2x-100.jpg" key={13} alt="Image 13" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 14@2x-100.jpg" key={14} alt="Image 14" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 15@2x-100.jpg" key={15} alt="Image 15" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 16@2x-100.jpg" key={16} alt="Image 16" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 17@2x-100.jpg" key={17} alt="Image 17" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 18@2x-100.jpg" key={18} alt="Image 18" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 19@2x-100.jpg" key={19} alt="Image 19" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 20@2x-100.jpg" key={20} alt="Image 20" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 21@2x-100.jpg" key={21} alt="Image 21" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 22@2x-100.jpg" key={22} alt="Image 22" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 23@2x-100.jpg" key={23} alt="Image 23" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 24@2x-100.jpg" key={24} alt="Image 24" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 25@2x-100.jpg" key={25} alt="Image 25" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 26@2x-100.jpg" key={26} alt="Image 26" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 27@2x-100.jpg" key={27} alt="Image 27" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 28@2x-100.jpg" key={28} alt="Image 28" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 29@2x-100.jpg" key={29} alt="Image 29" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 30@2x-100.jpg" key={30} alt="Image 30" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 31@2x-100.jpg" key={31} alt="Image 31" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 32@2x-100.jpg" key={32} alt="Image 32" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 33@2x-100.jpg" key={33} alt="Image 33" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 34@2x-100.jpg" key={34} alt="Image 34" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 35@2x-100.jpg" key={35} alt="Image 35" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 36@2x-100.jpg" key={36} alt="Image 36" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 37@2x-100.jpg" key={37} alt="Image 37" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 38@2x-100.jpg" key={38} alt="Image 38" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 39@2x-100.jpg" key={39} alt="Image 39" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 40@2x-100.jpg" key={40} alt="Image 40" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 41@2x-100.jpg" key={41} alt="Image 41" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 42@2x-100.jpg" key={42} alt="Image 42" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 43@2x-100.jpg" key={43} alt="Image 43" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 44@2x-100.jpg" key={44} alt="Image 44" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 45@2x-100.jpg" key={45} alt="Image 45" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 46@2x-100.jpg" key={46} alt="Image 46" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 47@2x-100.jpg" key={47} alt="Image 47" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 48@2x-100.jpg" key={48} alt="Image 48" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 49@2x-100.jpg" key={49} alt="Image 49" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 50@2x-100.jpg" key={50} alt="Image 50" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 51@2x-100.jpg" key={51} alt="Image 51" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 52@2x-100.jpg" key={52} alt="Image 52" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 53@2x-100.jpg" key={53} alt="Image 53" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 54@2x-100.jpg" key={54} alt="Image 54" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 55@2x-100.jpg" key={55} alt="Image 55" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 56@2x-100.jpg" key={56} alt="Image 56" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 57@2x-100.jpg" key={57} alt="Image 57" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 58@2x-100.jpg" key={58} alt="Image 58" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 59@2x-100.jpg" key={59} alt="Image 59" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+       <Image src="/images/flipbook/Artboard 60@2x-100.jpg" key={60} alt="Image 60" width={dimensions.width} height={dimensions.height} style={{objectFit:"cover"}}></Image>
+
+
+        {/* {imagePaths.map((src, index) => (
           <Image
+          src={"/images/flipbook/Artboard 1@2x-100.jpg"}
             key={index}
-            src={src}
             alt={`Image ${index + 1}`}
             width={dimensions.width}
             height={dimensions.height}
             style={{ objectFit: "cover" }}
           />
-        ))}
+        ))} */}
       </HTMLFlipBook>
     </div>
   );
