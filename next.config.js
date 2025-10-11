7fe272df3d73e265c45a54a86a6ad8ab3cf7884a
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-10-11 11:12:14 +0530

    removed css from global css to test page speed

--- next.config.js
+++ next.config.js
@@ -1,14 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: [
+//         'http://localhost:3001',
+//         'storage.googleapis.com',
+//         'https://timewatch2-0-311005204045.europe-west1.run.app'
+//       ],
+//     remotePatterns: [
+//       {
+//         protocol: 'http', 
+//         hostname: '*',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'http://localhost:3001',
-        'storage.googleapis.com',
-        'https://timewatch2-0-311005204045.europe-west1.run.app'
-      ],
+  reactStrictMode: true,
+  swcMinify: true, // faster minification, modern JS
+  experimental: {
+    legacyBrowsers: false, // don't transpile JS for old browsers
+  },
+  images: {
+    domains: [
+      'localhost', // remove http:// prefix
+      'storage.googleapis.com',
+      'timewatch2-0-311005204045.europe-west1.run.app',
+    ],
     remotePatterns: [
       {
-        protocol: 'http', 
+        protocol: 'http',
         hostname: '*',
       },
     ],
