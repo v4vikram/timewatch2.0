c95b4ff0682508e46819a6996b2b7d60b8dee8b3
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-10-31 17:04:35 +0530

    added qr nav and catalouge changes

--- src/app/(public)/products/layout.js
+++ src/app/(public)/products/layout.js
@@ -139,26 +139,26 @@ export default function CatLayout({ children }) {
       <div className="container">
         {/* Search bar */}
         <div className="flex justify-end gap-x-2 mb-3 relative">
-          <div className="relative">
+          <div className="relative w-full h-[38px]">
             <Input
               placeholder="Search products..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               className="w-full md:max-w-[260px] h-[38px] pr-10" // add padding for the clear button
             />
-                   {/* Clear button inside the input */}
-          {searchQuery && (
-            <button
-              onClick={() => setSearchQuery("")}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-              aria-label="Clear search"
-            >
-              <X />
-            </button>
-          )}
+            {/* Clear button inside the input */}
+            {searchQuery && (
+              <button
+                onClick={() => setSearchQuery("")}
+                className="absolute right-1 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                aria-label="Clear search"
+              >
+                <X />
+              </button>
+            )}
           </div>
 
-   
+
 
           {/* Existing filter toggle button */}
           <div className="flex justify-end xl:hidden mb-4">
