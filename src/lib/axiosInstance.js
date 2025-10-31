8160af5c915d4ea07ae5e1ae274c2353763a1b81
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-10-31 11:07:49 +0530

    changed backend base url

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:3001/api"
-  baseURL: "https://timewatch-dashboard-bk-311005204045.europe-west1.run.app/api",
+    baseURL: "https://timewatch-dashboard-bk-196626947975.asia-southeast1.run.app/api"
+  // baseURL: "https://timewatch-dashboard-bk-311005204045.europe-west1.run.app/api",
   // timeout: 10000,
 });
 
