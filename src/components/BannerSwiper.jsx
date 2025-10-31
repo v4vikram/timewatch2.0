d9231dde917cca2e8481695b187470065a50d8da
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-10-31 14:02:05 +0530

    added whatsapp icons

--- src/components/BannerSwiper.jsx
+++ src/components/BannerSwiper.jsx
@@ -28,18 +28,21 @@ export default function BannerSwiper() {
         // loop={false}
         className="w-full"
       >
-          <SwiperSlide>
+        {/* <SwiperSlide>
           <div className="bg-gray-50 overflow-hidden pt-14 md:pt-0">
             <div className="container h-[60vh] md:h-[100vh] 2xl:h-[calc(100vh-80px)] flex flex-col lg:flex-row items-center">
               <div className="flex-1 flex flex-col justify-center ">
                 <div className="max-w-[600px]">
                   <h1 className="text-3xl md:text-6xl font-bold text-primary">
-                    App Download Now<br/>
+                    App Download Now
+                    <br />
                     <span className="text-secondary"> Client Connect</span>
                   </h1>
                   <hr className="w-1/3 rounded-2xl h-1.5 mt-2 md:h-2 bg-primary border-0" />
                   <p className="text-md md:text-lg text-gray-600 leading-relaxed mt-5">
-                   Client Connect lets customers instantly log complaints, track progress anytime, and enjoy faster, transparent support.
+                    Client Connect lets customers instantly log complaints,
+                    track progress anytime, and enjoy faster, transparent
+                    support.
                   </p>
                   <Image
                     src="/images/sliders/home/client-search-bar.png"
@@ -91,19 +94,21 @@ export default function BannerSwiper() {
               </div>
             </div>
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <div className="bg-gray-50 overflow-hidden pt-14 md:pt-0">
             <div className="container h-[60vh] md:h-[100vh] 2xl:h-[calc(100vh-80px)] flex flex-col lg:flex-row items-center">
               <div className="flex-1 flex flex-col justify-center ">
                 <div className="max-w-[600px]">
                   <h1 className="text-3xl md:text-6xl font-bold text-primary">
-                  AI Based X-Ray
+                    AI Based X-Ray
                     <span className="text-secondary"> Baggage Scanner</span>
                   </h1>
                   <hr className="w-1/3 rounded-2xl h-1.5 mt-2 md:h-2 bg-primary border-0" />
                   <p className="text-md md:text-lg text-gray-600 leading-relaxed mt-5">
-                    We provide customized model solutions for our AI X-Ray baggage scanner, including software customization and barcode integration to meet your specific needs.
+                    We provide customized model solutions for our AI X-Ray
+                    baggage scanner, including software customization and
+                    barcode integration to meet your specific needs.
                   </p>
                 </div>
 
@@ -186,12 +191,15 @@ export default function BannerSwiper() {
               <div className="flex-1 flex flex-col justify-center ">
                 <div className="max-w-[600px]">
                   <h1 className="text-3xl md:text-6xl font-bold text-primary">
-                    Simple way to<br/>
+                    Simple way to
+                    <br />
                     <span className="text-secondary"> TRACK YOUR EMPLOYEE</span>
                   </h1>
                   <hr className="w-1/3 rounded-2xl h-1.5 mt-2 md:h-2 bg-primary border-0" />
                   <p className="text-md md:text-lg text-gray-600 leading-relaxed mt-5">
-                    Track employees in real time with geo-fencing, ensuring accurate attendance, productivity, and secure work monitoring.
+                    Track employees in real time with geo-fencing, ensuring
+                    accurate attendance, productivity, and secure work
+                    monitoring.
                   </p>
                 </div>
 
