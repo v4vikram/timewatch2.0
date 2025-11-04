7c937be7d61c3e43f1f2e3d3cd32fa8c9762e2f0
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-11-04 12:56:11 +0530

    changes in pages and added canonicals

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Link from "next/link";
 import { solutions } from "@/data/menuItems";
+import axiosInstance from "@/lib/axiosInstance";
 
 export const dynamic = "auto"; // optional; ISR will still work
 
