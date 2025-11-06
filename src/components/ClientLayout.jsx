d51494e470a4e7e72e6bbf4c7da0b0c3f18fa438
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-11-06 15:53:58 +0530

    added landing header in anathor pages

--- src/components/ClientLayout.jsx
+++ src/components/ClientLayout.jsx
@@ -9,10 +9,17 @@ import LandingHeader from "./LandingHeader";
 
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
-  console.log("pathname", pathname)
+  console.log("pathname", pathname);
 
   // ✅ Example: hide Header/Footer on specific pages
-  const hideLayout = ["/biometric-attendance-system-bio-1se-india", "/product-catalouge"].includes(pathname);
+  const hideLayout = [
+    "/biometric-attendance-system-bio-1se-india",
+    "/product-catalouge",
+    "/access-control-security-gate-manufacturer-in-India",
+    "/ai-face-t7hd",
+    "/biometric-attendance-access-control-system",
+    "/entrance-control-systems-secure-intelligent-pedestrian",
+  ].includes(pathname);
 
   useEffect(() => {
     // Example: scroll to top or analytics event
