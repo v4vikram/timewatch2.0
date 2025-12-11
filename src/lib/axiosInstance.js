fa9d13b03e39c041cf449b9df85c5d9218478d5d
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-12-11 11:28:05 +0530

    updated axios

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://72.60.202.56:3001/api"
+    baseURL: "https://www.timewatchindia.com/api/"
+    // baseURL: "http://72.60.202.56:3001/api"
     // baseURL: "http://localhost:3001/api"
     // baseURL: "https://timewatch-dashboard-bk-196626947975.asia-southeast1.run.app/api"
   // baseURL: "https://timewatch-dashboard-bk-311005204045.europe-west1.run.app/api",
